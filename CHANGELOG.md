c5b7f17919d905abc64852ab03ad8fe249268e63
Mike Lysikov <d8corp@mail.ru>
2022-03-23 20:32:19 +0300

    release v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,8 @@
 
 ## v0.1
 
+### v0.1.1 [![23.03.2022](https://img.shields.io/date/1648056660)](https://github.com/d8corp/watch-state/tree/v0.1.1)
+- added `GetProps` type
+
 ### v0.1.0 [![22.02.2022](https://img.shields.io/date/1645520585)](https://github.com/d8corp/watch-state/tree/v0.1.0)
 - first implementation
--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,12 @@
 export type Props = Record<string, any>
 export type Children = any[]
+export type RequiredKeys<T> = Exclude<{ [K in keyof T]-?: T extends { [K1 in K]: any } ? K : never}[keyof T], symbol>
+export type GetProps<
+  T,
+  R extends RequiredKeys<T> = RequiredKeys<T>,
+  O extends Exclude<keyof T, R | symbol> = Exclude<keyof T, R | symbol>
+> = (
+  Pick<T, R> | Record<`get:${R}`, T[R]>
+) & (
+  Partial<Pick<T, O>> | Partial<Record<`get:${O}`, T[O]>>
+)
